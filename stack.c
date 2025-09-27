70dab6eed5ccd0f4b0adc60b8eb2e137457b7e46
snftp <ale.drow@yandex.ru>
2025-09-27 22:06:30 +0300

    Removed asserts, added Stack_Dump, which redirects messeges into stack_log.txt

--- stack.c
+++ stack.c
@@ -1,53 +1,121 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
 #include "stack.h"
 
-void Stack_Init(stack_type *stack, size_t required_capacity) {
-    assert(stack != NULL);
-    stack->data = (int*)calloc(required_capacity, sizeof(int));
-    assert(stack->data != NULL);
-    stack->capacity = required_capacity;
-    stack->size = 0;
+#define POP_ERROR 0
+
+void Stack_Dump(FILE *log, stack_type *stack, const char *file_name, int file_line) {
+    if (log == NULL) {
+        return ;
+    }
+    fprintf(log, "Stack_Dump called from %s:%d\n", file_name, file_line);
+    if (stack == NULL) {
+        fprintf(log, "Stack[NULL]\n\n");
+        return ;
+    }
+    fprintf(log, "Stack[%p] {\n", (void*)stack);
+    fprintf(log, "  size     = %zu\n", stack->size);
+    fprintf(log, "  capacity = %zu\n", stack->capacity);
+    fprintf(log, "  data[%p] {\n", (void*)stack->data);
+    for (size_t i = 0; i < stack->capacity; ++i) {
+        int current_element = stack->data[i];
+        if (i < stack->size) {
+            fprintf(log, "    *[%zu] = %d\n", i, current_element);
+        }
+        else {
+            fprintf(log, "     [%zu] = %d\n",  i, 0);
+        }
+    }
+    fprintf(log, "  }\n");
+    fprintf(log, "}\n\n");
 }
 
-void Stack_Push(stack_type *stack, int value) {
-    assert(stack != NULL);
-    assert(stack->size <= stack->capacity);
+void Stack_Init(stack_type *stack, size_t required_capacity, FILE *log) {
+    if (stack == NULL) {
+        STACK_DUMP(log, NULL);
+        return ;
+    }
+    stack->data = (int*)calloc(required_capacity, sizeof(int));
+    if (stack->data == NULL) {
+        STACK_DUMP(log, stack);
+        return ;
+    }
+    stack->capacity = required_capacity;
+    stack->size = 0;
+    STACK_DUMP(log, stack);
+}
+
+void Stack_Push(stack_type *stack, int value, FILE *log) {
+    if (stack == NULL) {
+        STACK_DUMP(log, NULL);
+        return ;
+    }
+    if (stack->size > stack->capacity) {
+        fprintf(log, "ERROR: size(%zu) > capacity(%zu)\n", stack->size, stack->capacity);
+        STACK_DUMP(log, stack);
+        return;
+    }
     if (stack->capacity == stack->size) {
         int *new_memory = (int*)realloc(stack->data, stack->capacity * 2 * sizeof(int));
-        assert(new_memory != NULL);
+        if (new_memory == NULL) {
+            STACK_DUMP(log, stack);
+            return ;
+        }
         stack->data = new_memory;
         stack->capacity *= 2;
     }
     stack->data[stack->size] = value;
     stack->size++;
+    STACK_DUMP(log, stack);
 }
 
-int Stack_Pop(stack_type *stack) {
-    assert(stack != NULL);
-    assert(stack->size > 0);
+int Stack_Pop(stack_type *stack, bool *flag, FILE *log) {
+    if (stack == NULL) {
+        STACK_DUMP(log, NULL);
+        *flag = false;
+        return POP_ERROR;
+    }
+    if (stack->size == 0) {
+        fprintf(log, "ERROR: empty stack\n");
+        *flag = false;
+        STACK_DUMP(log, stack);
+        return POP_ERROR;
+    }
     stack->size--;
-    return stack->data[stack->size];
+    int value = stack->data[stack->size];
+    STACK_DUMP(log, stack);
+    return value;
 }
 
-void Stack_Destroy(stack_type *stack) {
-    assert(stack != NULL);
+void Stack_Destroy(stack_type *stack, FILE *log) {
+    if (stack == NULL) {
+        STACK_DUMP(log, NULL);
+        return ;
+    }
     free(stack->data);
     stack->size = 0;
     stack->capacity = 0;
+    STACK_DUMP(log, stack);
 }
 
+
 int main() {
+    bool flag = true;
+    FILE *log = fopen("stack_log.txt", "w");
+    if (log == NULL) {
+        return 1;
+    }
     stack_type stack = {};
-    Stack_Init(&stack, 5);
-    Stack_Push(&stack, 10);
-    Stack_Push(&stack, 20);
-    Stack_Push(&stack, 30);
-    printf("%d\n", Stack_Pop(&stack));
-    printf("%d\n", Stack_Pop(&stack));
-    printf("%d\n", Stack_Pop(&stack));
-    // printf("%d\n", Stack_Pop(&stack)); // NOTE: assert worked
-    Stack_Destroy(&stack);
+    Stack_Init(&stack, 5, log);
+    Stack_Push(&stack, 10, log);
+    Stack_Push(&stack, 20, log);
+    Stack_Push(&stack, 30, log);
+    printf("%d\n", Stack_Pop(&stack, &flag, log));
+    if (!flag) {
+        return 1;
+    }
+    Stack_Destroy(&stack, log);
+    fclose(log);
     return 0;
 }
--- stack.h
+++ stack.h
@@ -2,7 +2,7 @@
 #define STACK_H
 
 #include <stddef.h>
-#include <stdbool.h>
+#include <stdio.h>
 
 typedef struct {
     int *data;
@@ -10,10 +10,12 @@ typedef struct {
     size_t capacity;
 } stack_type;
 
-void Stack_Init(stack_type *stack, size_t required_capacity);
-void Stack_Push(stack_type *stack, int value);
-int Stack_Pop(stack_type *stack); // give the last added value
-void Stack_Destroy(stack_type *stack); // free data; size, capacity < 0
-// void Stack_Dump(stack_type *stack); // print current state of stack, errors, etc; not into console but some file
+void Stack_Init(stack_type *stack, size_t required_capacity, FILE *log);
+void Stack_Push(stack_type *stack, int value, FILE *log);
+int Stack_Pop(stack_type *stack, FILE *log);
+void Stack_Destroy(stack_type *stack, FILE *log);
+
+void Stack_Dump(FILE *log, stack_type *stack, const char *file_name, int file_line);
+#define STACK_DUMP(f, s) Stack_Dump((f), (s), __FILE__, __LINE__)
 
 #endif
